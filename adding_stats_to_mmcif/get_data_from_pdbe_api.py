9a301f223f9cdbad5a758df96b7ffbfcb5ff96f0
John Berrisford <jmb@ebi.ac.uk>
2018-07-15 22:16:43 +0100

    added get data from pdbe api
    added tests
    
    added specific call to get one_letter_code for a ligand
    
    added tests

---
+++ adding_stats_to_mmcif/get_data_from_pdbe_api.py
@@ -0,0 +1,92 @@
+import re
+import requests
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+import logging
+import argparse
+
+logger = logging.getLogger()
+FORMAT = "%(filename)s - %(funcName)s - %(message)s"
+logging.basicConfig(format=FORMAT)
+
+
+BASE_URL = 'https://www.ebi.ac.uk/pdbe/api/'
+API_END_POINTS = {'compounds': 'pdb/compound/summary/'}
+
+class GetDataFromPdbeAPi:
+
+    def __init__(self, entry_id, end_point, server_root=BASE_URL):
+        self.url = None
+        self.entry_id = entry_id
+        self.suffix = end_point
+        self.server_root = server_root
+        self.data = {}
+
+        self.url_from_suffix()
+        self.get_data()
+
+    def url_from_suffix(self):
+        if self.suffix in API_END_POINTS and self.entry_id and self.server_root:
+            suffix = API_END_POINTS[self.suffix]
+            self.url = self.server_root + suffix + self.entry_id
+        else:
+            logging.error('unknown PDBe API call')
+
+    def get_data(self):
+        if self.url:
+            self.encode_url()
+
+            s = requests.Session() # start a requests session
+            retries = Retry(total=5, # number of retries
+                            backoff_factor=1.0, # the factor time in seconds is multiplied by before a retry is tried again
+                            status_forcelist=[500, 502, 503, 504]) # retry for these status codes
+            s.mount('http://', HTTPAdapter(max_retries=retries)) # retry for these protocols.
+            s.mount('https://', HTTPAdapter(max_retries=retries))
+
+            r = s.get(url=self.url, timeout=60)
+            # r = requests.get(url=self.url, timeout=60)
+
+            if r.status_code == 200:
+                json_data = r.json()
+                if self.entry_id in json_data:
+                    self.data = json_data[self.entry_id]
+                else:
+                    self.data = json_data
+            elif r.status_code == 404:
+                self.data = {}
+
+            else:
+                print(r.status_code, r.reason)
+                self.data = {}
+
+    def return_data(self):
+        return self.data
+
+    def encode_url(self):
+        self.url = re.sub(" ", "%20", self.url)
+
+
+class GetSpecificDataFromPdbeAPI:
+
+    def get_one_letter_code_for_compound(self, compound):
+        one_letter_code = 'X'
+        if compound:        
+            compound = compound.upper()
+            pdbe_api_data = GetDataFromPdbeAPi(entry_id=compound, end_point='compounds').return_data()
+            if pdbe_api_data:
+                for data in pdbe_api_data:
+                    one_letter_code = data['one_letter_code']
+        return one_letter_code
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--entry_id', help='the entry to query', type=str, required=True)
+    parser.add_argument('--api_end_point', help='the api end point', type=str, required=True)
+    parser.add_argument('-d', '--debug', help='debugging', action='store_const', dest='loglevel', const=logging.DEBUG,
+                        default=logging.INFO)
+    args = parser.parse_args()
+
+    logger.setLevel(args.loglevel)
+
+    pdbe_api_data = GetDataFromPdbeAPi(entry_id=args.entry_id, end_point=args.api_end_point).return_data()
+    logging.info(pdbe_api_data)
---
+++ tests/test_get_data_from_pdbe_api.py
@@ -0,0 +1,55 @@
+import unittest
+from adding_stats_to_mmcif.get_data_from_pdbe_api import GetDataFromPdbeAPi, GetSpecificDataFromPdbeAPI
+
+class TestAddDataFromAimless(unittest.TestCase):
+
+    def test_null_entry(self):
+        s = GetDataFromPdbeAPi(entry_id=None, end_point='compounds').return_data()
+        self.assertEqual(s, dict())
+
+    def test_null_server(self):
+        s = GetDataFromPdbeAPi(entry_id='ATP', end_point='compounds', server_root=None).return_data()
+        self.assertEqual(s, dict())
+
+    def test_null_end_point(self):
+        s = GetDataFromPdbeAPi(entry_id='ATP', end_point=None).return_data()
+        self.assertEqual(s, dict())
+
+    def test_unknown_end_point(self):
+        s = GetDataFromPdbeAPi(entry_id='ATP', end_point='cheese').return_data()
+        self.assertEqual(s, dict())
+
+    def test_unknown_entry_id(self):
+        s = GetDataFromPdbeAPi(entry_id='XXXXXXXXXXX', end_point='compounds').return_data()
+        self.assertEqual(s, dict())
+    
+    def test_known_entry_id(self):
+        s = GetDataFromPdbeAPi(entry_id='ATP', end_point='compounds').return_data()
+        self.assertNotEqual(s, dict())
+
+    def test_known_lower_case_entry_id(self):
+        s = GetDataFromPdbeAPi(entry_id='atp', end_point='compounds').return_data()
+        self.assertEqual(s, dict())
+
+    def test_get_one_letter_code_for_tpo(self):
+        s = GetSpecificDataFromPdbeAPI().get_one_letter_code_for_compound(compound='TPO')
+        self.assertEqual(s, 'T')
+
+    def test_get_one_letter_code_for_tpo_lower(self):
+        s = GetSpecificDataFromPdbeAPI().get_one_letter_code_for_compound(compound='tpo')
+        self.assertEqual(s, 'T')
+
+    def test_get_one_letter_code_for_ATP(self):
+        s = GetSpecificDataFromPdbeAPI().get_one_letter_code_for_compound(compound='ATP')
+        self.assertEqual(s, 'X')
+
+    def test_get_one_letter_code_for_unknown_ligand(self):
+        s = GetSpecificDataFromPdbeAPI().get_one_letter_code_for_compound(compound='XXXXXXXXXXX')
+        self.assertEqual(s, 'X')
+
+    def test_get_one_letter_code_for_None_ligand(self):
+        s = GetSpecificDataFromPdbeAPI().get_one_letter_code_for_compound(compound=None)
+        self.assertEqual(s, 'X')
+
+if __name__ == '__main__':
+    unittest.main()

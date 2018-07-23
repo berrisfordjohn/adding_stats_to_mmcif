5722343d88fd6abf31400938f71d3850b98069f8
John Berrisford <jmb@ebi.ac.uk>
2018-07-23 21:43:30 +0100

    moved wwpdb_validation_tests

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-from setuptools import setup, find_packages 
+from setuptools import setup, find_packages
 
 setup(
     name='adding_stats_to_mmcif',
     version='0.1',
     url='https://github.com/berrisfordjohn/adding_stats_to_mmcif',
     author='John Berrisford',
-    test_suite = 'tests',
+    test_suite='tests',
     zip_safe=True,
     packages=find_packages('adding_stats_to_mmcif'),
-    #dependency_links=['https://github.com/project-gemmi/gemmi/tarball/master#egg=gemmi'],
-    install_requires=['biopython>=1.72', 'requests', 'onedep_api'],
+    # dependency_links=['https://github.com/project-gemmi/gemmi/tarball/master#egg=gemmi'],
+    install_requires=['biopython>=1.72',
+                      'requests',
+                      # 'onedep_api'
+                      ],
 )

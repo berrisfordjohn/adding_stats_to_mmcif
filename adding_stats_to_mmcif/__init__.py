fdf8e4b156966fb16ca09cedce029780a20cf1ed
John Berrisford <berrisfordjohn@gmail.com>
2019-01-14 21:38:22 +0000

    added option to get data from an mmCIF file to __init__

--- adding_stats_to_mmcif/__init__.py
+++ adding_stats_to_mmcif/__init__.py
@@ -9,9 +9,10 @@ logging.basicConfig(format=FORMAT)
 
 from .add_sequence_to_mmcif import AddSequenceToMmcif
 from .add_data_from_aimless_xml import run_process as addAimlessDataToMmcif
+from .add_data_from_mmcif import AddToMmcif
 
 
-def run_process(input_mmcif, output_mmcif, fasta_file, xml_file=None):
+def run_process(input_mmcif, output_mmcif, fasta_file, xml_file=None, input_mmcif_to_get_data_from=None):
 
     worked = True
 
@@ -22,6 +23,11 @@ def run_process(input_mmcif, output_mmcif, fasta_file, xml_file=None):
             worked = addAimlessDataToMmcif(xml_file=xml_file, input_cif=input_mmcif, output_cif=temp_cif)
             if not worked:
                 logging.error('adding statistics to mmCIF failed')
+        elif input_mmcif_to_get_data_from:
+            ac = AddToMmcif()
+            data = ac.get_data(input_mmcif_to_get_data_from)
+            if data:
+                worked = ac.add_to_cif(input_mmcif_file=input_mmcif, output_mmcif_file=temp_cif, data_dictionary=data)
         else:
             shutil.copy(input_mmcif, temp_cif)
 

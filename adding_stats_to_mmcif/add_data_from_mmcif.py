b769d1a8a25c85d20f52f11fbb046a6916a2ef53
John Berrisford <berrisfordjohn@gmail.com>
2019-01-14 21:30:45 +0000

    add test for adding data from mmcif

--- adding_stats_to_mmcif/add_data_from_mmcif.py
+++ adding_stats_to_mmcif/add_data_from_mmcif.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from .cif_handling import mmcifHandling
+from adding_stats_to_mmcif.cif_handling import mmcifHandling
 import argparse
 import logging
 
@@ -8,60 +8,30 @@ FORMAT = "%(filename)s - %(funcName)s - %(message)s"
 logging.basicConfig(format=FORMAT)
 
 
-def run_process(mmcif_to_add_data_to, input_cif):
-    gc = GetCifData(input_cif)
-    gc.run_process()
-    data = gc.return_data()
+class AddToMmcif:
 
-    ac = AddToMmcif()
-    ac.parse_mmcif(mmcif_file=mmcif_to_add_data_to)
-    ac.add_to_cif(mmcif_file=mmcif_to_add_data_to, data_dictionary=data)
-
-
-class GetCifData:
-
-    def __init__(self, mmcif_file):
-        self.mmcif_file = mmcif_file
+    def __init__(self):
         self.mmcif_data = dict()
         self.ch = mmcifHandling()
 
-    def parse_cif(self):
-        return self.ch.parse_mmcif(self.mmcif_file)
-
-    def get_datablocks(self):
-        return self.ch.getDatablocks()
-
-    def get_datablock(self, datablock):
-        if datablock in self.get_datablocks():
-            return self.ch.getDatablock(datablock=datablock)
-        return None
-
-    def get_categories(self):
-        return self.ch.getCategories()
-
-    def run_process(self):
-        if self.parse_cif():
-            for datablock in self.get_datablocks():
-                if self.get_datablock(datablock=datablock):
-                    for category in self.get_categories():
+    def get_data(self, mmcif_file):
+        if self.ch.parse_mmcif(fileName=mmcif_file):
+            for datablock in self.ch.getDatablocks():
+                if self.ch.getDatablock(datablock=datablock):
+                    for category in self.ch.getCategories():
                         self.mmcif_data.update(self.ch.getCategory(category=category))
 
-    def return_data(self):
         return self.mmcif_data
 
-
-class AddToMmcif:
-
-    def _init__(self):
-        self.ch = mmcifHandling()
-
-    def parse_mmcif(self, mmcif_file):
-        self.ch.parse_mmcif(mmcif_file)
-        return self.ch.getDatablock(datablock=0)
-
-    def add_to_cif(self, mmcif_file, data_dictionary):
-        self.ch.addToCif(data_dictionary=data_dictionary)
-        self.ch.writeCif(fileName=mmcif_file)
+    def add_to_cif(self, input_mmcif_file, output_mmcif_file, data_dictionary):
+        try:
+            self.ch.parse_mmcif(fileName=input_mmcif_file)
+            self.ch.addToCif(data_dictionary=data_dictionary)
+            self.ch.writeCif(fileName=output_mmcif_file)
+            return True
+        except Exception as e:
+            logging.error(e)
+            return False
 
 
 if __name__ == '__main__':
@@ -75,4 +45,10 @@ if __name__ == '__main__':
 
     logger.setLevel(args.loglevel)
 
-    run_process(mmcif_to_add_data_to=args.mmcif_to_add_data_to, input_cif=args.input_mmcif)
+    ret = False
+    gc = AddToMmcif()
+    data = gc.get_data(args.input_mmcif)
+    if data:
+        ret = gc.add_to_cif(input_mmcif_file=args.mmcif_to_add_data_to, output_mmcif_file=args.mmcif_to_add_data_to,
+                            data_dictionary=data)
+    logging.info('worked: {}'.format(ret))
